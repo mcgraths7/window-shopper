6bb6907a1f0917492182c24cf28bd83e49798cfe
mcgraths7 <mcgrath.steven7@gmail.com>
2018-09-05 13:35:03 -0400

    Placed user interface and built custom text field class

---
+++ window-shopper/window-shopper/View/CurrencyTextField.swift
@@ -0,0 +1,34 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by Steven McGrath on 9/5/18.
+//  Copyright © 2018 Steven McGrath. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 4
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.652531036)
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let placehld = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = placehld
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
